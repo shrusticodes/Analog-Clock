e0ce4092826257dcc0adc0941bd52c1a1da08dea
Shrusti <89652827+shrusticodes@users.noreply.github.com>
2023-06-01 12:39:19 +0530

    wraparound effect removed

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const hours = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const second = document.querySelector('.second');
 const clockDiv = document.querySelector('.clock');
+const revolutions = 0;
+const lastAngle = 0;
 createDivs();
 setInterval(() => {
   const date = new Date();
   const secDegree = 6 * date.getSeconds();
   const minDegree = 6 * date.getMinutes();
   const hourDegree = 30 * date.getHours() + date.getMinutes() / 2;
+  if (secDegree < lastAngle)
+  revolutions = revolutions + 1;
   hours.style.transform = `rotate(${hourDegree}deg)`;
   min.style.transform = `rotate(${minDegree}deg)`;
   second.style.transform = `rotate(${secDegree}deg)`;
