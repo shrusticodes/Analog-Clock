2802fe9b2ee7c829125a5b9f7b2297f9d260a7bc
Shrusti <89652827+shrusticodes@users.noreply.github.com>
2023-06-01 17:30:53 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import './style.css';
 const hours = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const second = document.querySelector('.second');
@@ -23,17 +24,32 @@ setInterval(() => {
 
 function createDivs() {
   const numbersDiv = document.createElement('div');
+  // let minutesDiv, mainMinutesDiv;
+  // let k = 1;
   for (let i = 1; i <= 12; i++) {
     const newDiv = document.createElement('div');
     newDiv.className = `number number${i}`;
     let innerDiv = document.createElement('div');
     innerDiv.innerText = i;
-    for (let j=1;j<=4;j++)
-    {
-      
-    }
     newDiv.appendChild(innerDiv);
     numbersDiv.appendChild(newDiv);
+    // for (let j = 1; j <= 4; j++) {
+    //   minutesDiv = document.createElement('div');
+    //   mainMinutesDiv = document.createElement('div');
+    //   mainMinutesDiv.className = `minutes`;
+    //   minutesDiv.className = `minutes${k}`;
+    //   let eachMinDegree = 6 * k;
+    //   if (eachMinDegree % 5 == 0) {
+    //     k++;
+    //     eachMinDegree = 6 * k;
+    //     mainMinutesDiv.style.transform = `rotate(${eachMinDegree})`;
+    //   }
+    //   console.log(eachMinDegree);
+    //   console.log(k);
+    //   k++;
+    //   mainMinutesDiv.appendChild(minutesDiv);
+    //   numbersDiv.appendChild(mainMinutesDiv);
+    // }
   }
   clockDiv.appendChild(numbersDiv);
 }
