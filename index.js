5d1b93f1e47e9e2fe61e670793949d9bd2dbbc8c
shrusticodes <89652827+shrusticodes@users.noreply.github.com>
2023-06-02 18:11:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import './style.css';
+import './style.css';
 const hours = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const second = document.querySelector('.second');
@@ -44,7 +44,7 @@ function createDivs() {
       innerDiv.style.transform = `rotate(${-i * 6}deg)`;
     } else {
       const minDiv = document.createElement('div');
-      minDiv.className = "minutes";
+      minDiv.className = 'minutes';
       minDiv.style.margin = 'auto';
       minDiv.style.width = '1.5px';
       minDiv.style.height = '7px';
@@ -57,52 +57,44 @@ function createDivs() {
   }
   clockDiv.appendChild(numbersDiv);
 }
-function createButton()
-{
+function createButton() {
   const button = document.createElement('button');
-  button.style.marginLeft = "20%";
-  button.style.marginTop = "30px";
-  button.innerText="Change Mode";
-  button.className="light-mode";
-  button.style.borderRadius="10px";
-  button.addEventListener("click", setMode);
+  button.style.marginLeft = '20%';
+  button.style.marginTop = '30px';
+  button.innerText = 'Change Mode';
+  button.className = 'light-mode';
+  button.style.borderRadius = '10px';
+  button.addEventListener('click', setMode);
   mainDiv.appendChild(button);
 }
-function setMode()
-{
-  console.log("button clicked")
-  const buttonElement = document.querySelector("button");
+function setMode() {
+  console.log('button clicked');
+  const buttonElement = document.querySelector('button');
   const body = document.body;
-  let minutes = document.getElementsByClassName('minutes')
-  if ( buttonElement.className=="light-mode"){
-  buttonElement.className="dark-theme";
-  body.className="dark";
-  clockDiv.className = "clock dark-theme";
-  clockDiv.style.border = `7px solid rgb(215, 213, 213)`
-  hours.style.backgroundColor = `hsl(224, 42%, 57%)`;
-  min.style.backgroundColor = `hsl(224, 42%, 57%)`;
-  second.style.backgroundColor = `rgb(215, 213, 213)`;
-  clockDiv.style.setProperty("--circleColor", `hsl(224, 42%, 57%)`);
-  for(let i=0;i<minutes.length;i++)
-  {
-    minutes[i].style.backgroundColor = `rgb(215, 213, 213)`;
+  let minutes = document.getElementsByClassName('minutes');
+  if (buttonElement.className == 'light-mode') {
+    buttonElement.className = 'dark-theme';
+    body.className = 'dark';
+    clockDiv.className = 'clock dark-theme';
+    clockDiv.style.border = `7px solid rgb(215, 213, 213)`;
+    hours.style.backgroundColor = `hsl(224, 42%, 57%)`;
+    min.style.backgroundColor = `hsl(224, 42%, 57%)`;
+    second.style.backgroundColor = `rgb(215, 213, 213)`;
+    clockDiv.style.setProperty('--circleColor', `hsl(224, 42%, 57%)`);
+    for (let i = 0; i < minutes.length; i++) {
+      minutes[i].style.backgroundColor = `rgb(215, 213, 213)`;
+    }
+  } else {
+    buttonElement.className = 'light-mode';
+    body.className = 'light';
+    clockDiv.className = 'clock light-mode';
+    hours.style.backgroundColor = 'black';
+    min.style.backgroundColor = 'black';
+    clockDiv.style.border = `7px solid black`;
+    second.style.backgroundColor = 'red';
+    clockDiv.style.setProperty('--circleColor', `black`);
+    for (let i = 0; i < minutes.length; i++) {
+      minutes[i].style.backgroundColor = `black`;
+    }
   }
-  }
-  else{
-  buttonElement.className="light-mode";
-  body.className="light";
-  clockDiv.className = "clock light-mode";
-  hours.style.backgroundColor = "black";
-  min.style.backgroundColor = "black";
-  clockDiv.style.border = `7px solid black`;
-  second.style.backgroundColor = "red";
-  clockDiv.style.setProperty("--circleColor", `black`);
-  for(let i=0;i<minutes.length;i++)
-  {
-    minutes[i].style.backgroundColor = `black`;
-  }
-  minutes.forEach(element => {
-    element.style.backgroundColor = 'black';
-  });
-}
 }
