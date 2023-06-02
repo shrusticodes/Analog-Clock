4a7a669b9342c89d7c6744d91d5c9c68a1dcebf3
shrusticodes <89652827+shrusticodes@users.noreply.github.com>
2023-06-02 09:47:39 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,29 +3,34 @@ const hours = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const second = document.querySelector('.second');
 const clockDiv = document.querySelector('.clock');
-let revolutions = 0;
-let lastAngle = 0;
+let secRevolutions = 0;
+let secLastAngle = 0;
+let minRevolutions = 0;
+let minLastAngle = 0;
+let hourRevolutions = 0;
+let hourLastAngle = 0;
 createDivs();
 setInterval(() => {
   const date = new Date();
   const secDegree = 6 * date.getSeconds();
   const minDegree = 6 * date.getMinutes();
   const hourDegree = 30 * date.getHours() + date.getMinutes() / 2;
-  if (secDegree < lastAngle) revolutions = revolutions + 1;
-  // console.log(revolutions);
-  // console.log(secDegree);
-  // console.log(lastAngle);
-  let secondDegreeFinal = revolutions * 360 + secDegree;
-  lastAngle = secDegree;
-  hours.style.transform = `rotate(${hourDegree}deg)`;
-  min.style.transform = `rotate(${minDegree}deg)`;
+  if (secDegree < secLastAngle) secRevolutions = secRevolutions + 1;
+  if (minDegree < minLastAngle) minRevolutions = minRevolutions + 1;
+  if (hourDegree < hourLastAngle) hourRevolutions = hourRevolutions + 1;
+  let secondDegreeFinal = secRevolutions * 360 + secDegree;
+  let minDegreeFinal = minRevolutions * 360 + minDegree;
+  let hourDegreeFinal = hourRevolutions * 360 + hourDegree;
+  secLastAngle = secDegree;
+  minLastAngle = minDegree;
+  hourLastAngle = hourDegree;
+  hours.style.transform = `rotate(${hourDegreeFinal}deg)`;
+  min.style.transform = `rotate(${minDegreeFinal}deg)`;
   second.style.transform = `rotate(${secondDegreeFinal}deg)`;
 }, 1000);
 
 function createDivs() {
   const numbersDiv = document.createElement('div');
-  let mainMinutesDiv = document.createElement('div');
-  let k = 1;
   for (let i = 1; i <= 12; i++) {
     const newDiv = document.createElement('div');
     newDiv.className = `number number${i}`;
@@ -33,23 +38,26 @@ function createDivs() {
     innerDiv.innerText = i;
     newDiv.appendChild(innerDiv);
     numbersDiv.appendChild(newDiv);
-    }
-    // for (let j = 1; j <= 4; j++) {
-    //   minutesDiv = document.createElement('div');
-    //   mainMinutesDiv = document.createElement('div');
-    //   mainMinutesDiv.className = `minutes`;
-    //   minutesDiv.className = `minutes${k}`;
-    //   let eachMinDegree = 6 * k;
-    //   if (eachMinDegree % 5 == 0) {
-    //     k++;
-    //     eachMinDegree = 6 * k;
-    //     mainMinutesDiv.style.transform = `rotate(${eachMinDegree})`;
-    //   }
-    //   console.log(eachMinDegree);
-    //   console.log(k);
-    //   k++;
-    //   mainMinutesDiv.appendChild(minutesDiv);
-    //   numbersDiv.appendChild(mainMinutesDiv);
-    // }
   }
+
   clockDiv.appendChild(numbersDiv);
+}
+
+
+  // for (let j = 1; j <= 4; j++) {
+  //   minutesDiv = document.createElement('div');
+  //   mainMinutesDiv = document.createElement('div');
+  //   mainMinutesDiv.className = `minutes`;
+  //   minutesDiv.className = `minutes${k}`;
+  //   let eachMinDegree = 6 * k;
+  //   if (eachMinDegree % 5 == 0) {
+  //     k++;
+  //     eachMinDegree = 6 * k;
+  //     mainMinutesDiv.style.transform = `rotate(${eachMinDegree})`;
+  //   }
+  //   console.log(eachMinDegree);
+  //   console.log(k);
+  //   k++;
+  //   mainMinutesDiv.appendChild(minutesDiv);
+  //   numbersDiv.appendChild(mainMinutesDiv);
+  // }
